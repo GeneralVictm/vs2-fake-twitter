29aecb7d4386e0ec384afe8b836463bd36b03718
GeneralVictm <sascha.ehrler@yahoo.de>
2017-05-09 15:29:35 +0200

    added config file and extended config class, not finished!

--- src/main/java/de/hska/vs2/FakeTwitterConfig.java
+++ src/main/java/de/hska/vs2/FakeTwitterConfig.java
@@ -1,8 +1,20 @@
+package de.hska.vs2;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+
+@Configuration
 public class FakeTwitterConfig {
 
+	// Klasse ist so noch nicht vollständig, für die Template-Methoden fehlt in dieser Form noch weiterer Input, ggfs. auch weitere Imports!!!
+	// siehe hierzu Kapitel 4.3 des Thymeleaf-Tutorials
+
 	@Bean
-	public SpringResourceTemplateResolver templateResolver(){
+	public SpringResourceTemplateResolver templateResolver() {
 	    // SpringResourceTemplateResolver automatically integrates with Spring's own
 	    // resource resolution infrastructure, which is highly recommended.
 	    SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
@@ -10,7 +22,7 @@ public class FakeTwitterConfig {
 	    templateResolver.setPrefix("/WEB-INF/templates/");
 	    templateResolver.setSuffix(".html");
 	    // HTML is the default value, added here for the sake of clarity.
-	    templateResolver.setTemplateMode(TemplateMode.HTML);
+	    templateResolver.setTemplateMode("HTML5");
 	    // Template cache is true by default. Set to false if you want
 	    // templates to be automatically updated when modified.
 	    templateResolver.setCacheable(true);
@@ -18,7 +30,7 @@ public class FakeTwitterConfig {
 	}
 
 	@Bean
-	public SpringTemplateEngine templateEngine(){
+	public SpringTemplateEngine templateEngine() {
 	    // SpringTemplateEngine automatically applies SpringStandardDialect and
 	    // enables Spring's own MessageSource message resolution mechanisms.
 	    SpringTemplateEngine templateEngine = new SpringTemplateEngine();
@@ -31,5 +43,31 @@ public class FakeTwitterConfig {
 	    templateEngine.setEnableSpringELCompiler(true);
 	    return templateEngine;
 	}
-	
+
+	/**
+	 * JedisConnectionFactory getConnectionFactory()
+	 *
+	 * creates a standard jedis connection for working with a local redis
+	 */
+	@Bean
+	public JedisConnectionFactory getConnectionFactory() {
+		JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
+		jedisConnectionFactory.setHostName("localhost");
+		jedisConnectionFactory.setPort(6379);
+		jedisConnectionFactory.setPassword("");
+		return jedisConnectionFactory;
+	}
+
+	/**
+	 * defines base redis template
+	 *
+	 * @return template
+	 */
+	@Bean(name= "redisTemplate")
+	RedisTemplate< String, Object > redisTemplate() {
+		final RedisTemplate< String, Object > template = new RedisTemplate< String, Object >();
+		template.setConnectionFactory( getConnectionFactory() );
+		return template;
+	}
+
 }
